853d22a3a3af1ba62a82105c5e6490024250a4f2
efekos <hefeolgun10@gmail.com>
2023-01-08 19:45:07 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-const BadgeService = game.GetService("BadgeService");
+const images:ImageList = {}
 
+/**
+* An Image Service is to store image ids with names so you don't have to remember all ids every time.
+* 
+* Since it not saves the images to a global value (yet), it should be identified in a module and imported from there in every use.
+*/
 export class ImageService{
-    private images:ImageList
-
-
-    /**
-    * An Image Service is to store image ids with names so you don't have to remember all ids every time.
-    * 
-    * Since it not saves the images to a global value (yet), it should be identified in a module and imported from there in every use.
-    */
-    constructor(){
-        this.images = {}
-    }
 
     /**
      * Adds an image. you can use this with {@link ImageService.getImage()}.
@@ -20,10 +14,10 @@ export class ImageService{
      * @returns Whenever the image is added succesfully or not.
      */
     addImage(name:string,id:`rbxassetid://${string}` | number):boolean {
-        if(this.images[name] === undefined) return false;
+        if(images[name] === undefined) return false;
 
         try {
-            this.images[name] = id as string
+            images[name] = id as string
             return true
         } catch {
             return false
@@ -36,26 +30,26 @@ export class ImageService{
      * @returns Image id if found.
      */
     getImage(name:string):string | undefined{
-        return this.images[name]
+        return images[name]
     }
 
     /**
      * @returns All images stored to this ImageService.
      */
     getAllImages():ImageList {
-        return this.images
+        return images
     }
 
     /**
      * Removes an image.
      * @param name Name of the image.
-     * @returns Whenever the image is removed successfully or not.
+     * @returns Whether the image was successfully removed
      */
     removeImage(name:string):boolean {
-        if(this.images[name] === undefined) return false;
+        if(images[name] === undefined) return false;
 
         try{
-            delete this.images[name]
+            delete images[name]
             return true;
         } catch {
             return false;
